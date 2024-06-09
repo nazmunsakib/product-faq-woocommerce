d2b7ac82dc008405b50960d4114d8056962c8d57
Sakib <dynamicteam2@proton.me>
2024-06-09 23:32:19 +0600

    Initial plugin setup

---
+++ product-faq-woocommerce.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Plugin Name: Product FAQ for woocommerce
+ * Plugin URI: https://nazmunsakib.co/
+ * Description: Dynamic Event Management plugin!
+ * Version: 1.0.0
+ * Author: Nazmun Sakib
+ * Author URI: https://nazmunsakib.com
+ * License: GPL2
+ * Text Domain: product-faq-woocommerce
+ * Domain Path: /languages
+ * 
+ * WP Requirement & Test
+ * Requires at least: 4.4
+ * Tested up to: 6.5
+ * Requires PHP: 5.6
+ * 
+ * WC Requirement & Test
+ * WC requires at least: 3.2
+ * WC tested up to: 7.9
+ * 
+ *  @package ProductFaqWoocommerce
+ */
+
+
+defined('ABSPATH') || die();
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
+/**
+ * Main class for Dynamic Event Management plugin.
+ */
+final class Product_FAQ_Woocommerce {
+
+    /**
+     * The single instance of the class.
+     *
+     * @var Product_FAQ_Woocommerce|null
+     */
+    private static $instance = null;
+
+    /**
+     * Plugin version.
+     *
+     * @var string
+     */
+    private static $version = '1.0.0';
+
+    /**
+     * Constructor.
+     *
+     * Initializes the class and hooks necessary actions.
+     */
+    private function __construct() {
+        $this->define_constants();
+        $this->add_hooks();
+    }
+
+    /**
+     * Returns the single instance of the class.
+     *
+     * @return Product_FAQ_Woocommerce The single instance of the class.
+     */
+    public static function instance() {
+        if ( is_null( self::$instance ) ) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Defines plugin constants.
+     */
+    private function define_constants() {
+        define( 'PRODUCT_FAQ_WOO_VERSION', self::$version );
+        define( 'PRODUCT_FAQ_WOO_FILE', __FILE__ );
+        define( 'PRODUCT_FAQ_WOO_PATH', __DIR__ );
+        define( 'PRODUCT_FAQ_WOO_URL', plugins_url( '', PRODUCT_FAQ_WOO_FILE ) );
+        define( 'PRODUCT_FAQ_WOO_ASSETS', PRODUCT_FAQ_WOO_URL . '/assets' );
+    }
+
+    /**
+     * Adds hooks.
+     */
+    private function add_hooks() {
+        add_action( 'init', array( $this, 'load_textdomain' ) );
+        add_action( 'plugins_loaded', array( $this, 'init' ) );
+    }
+
+    /**
+     * Initializes the plugin.
+     */
+    public function init() {
+        $this->includes();
+    }
+
+    /**
+     * Includes necessary files.
+     */
+    private function includes() {
+
+    }
+
+    /**
+     * Loads the plugin's text domain for localization.
+     */
+    public function load_textdomain() {
+        load_plugin_textdomain( 'product-faq-woocommerce', false, dirname( plugin_basename( PRODUCT_FAQ_WOO_FILE ) ) . '/languages' );
+    }
+
+}
+
+/**
+ * Initializes the Product_FAQ_Woocommerce class.
+ *
+ * @return Product_FAQ_Woocommerce
+ */
+function product_faq_woocommerce() {
+    return Product_FAQ_Woocommerce::instance();
+}
+
+// Initialize the plugin.
+product_faq_woocommerce();
